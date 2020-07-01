7d4675adf7ef343fd5712cffc48647ed40864cb1
Saswati Mishra <saswatimishra140@gmail.com>
2020-07-01 21:47:16 +0530

    2nd

--- lib/pages/todo.dart
+++ lib/pages/todo.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TodoPage extends StatefulWidget {
-  var _formkey = GlobalKey<FormState>();
   @override
   _TodoPageState createState() => _TodoPageState();
 }
 
 class _TodoPageState extends State<TodoPage> {
+  var _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +20,31 @@ class _TodoPageState extends State<TodoPage> {
             Dialog(
               child: Form(
                 key: _formkey,
-                
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: TextFormField(
+                        decoration:
+                            InputDecoration(hintText: "Enter Your Task."),
+                        validator: (value) =>
+                            value.isEmpty ? "please enter something" : null,
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: null,
+                      child: Text("Submit"),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
+        onPressed: () {},
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
